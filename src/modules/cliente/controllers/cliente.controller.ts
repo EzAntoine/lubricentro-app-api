6189407d417af7d585f0dee441129aea5c14a314
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-07-17 01:11:52 -0300

    feat: Client controller

--- src/modules/cliente/controllers/cliente.controller.ts
+++ src/modules/cliente/controllers/cliente.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ClienteService } from '../services/cliente.service';
+import { MongoIdPipe } from 'src/common/mongo-id.pipe';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { CreateClientDto, UpdateClientDTO } from '../dtos/client.dto';
+@ApiTags('Clients')
 @Controller('cliente')
-export class ClienteController {}
+export class ClienteController {
+  constructor(private clientService: ClienteService) {}
+
+  @ApiOperation({ summary: 'Get all clients.' })
+  @Get()
+  getClients() {
+    return this.clientService.findAll();
+  }
+
+  @ApiOperation({ summary: 'Get client by ID.' })
+  @Get(':id')
+  getClientById(@Param('id') id: string) {
+    return this.clientService.findById(id);
+  }
+
+  @ApiOperation({ summary: 'Create new client.' })
+  @Post()
+  createClient(@Body() payload: CreateClientDto) {
+    return this.clientService.create(payload);
+  }
+
+  @ApiOperation({ summary: 'Update an existing client by ID.' })
+  @Put(':id')
+  updateClient(
+    @Param('id', MongoIdPipe) id: string,
+    @Body() payload: UpdateClientDTO,
+  ) {
+    return this.clientService.update(id, payload);
+  }
+
+  @ApiOperation({ summary: 'Remove a client by ID.' })
+  @Delete(':id')
+  deleteClient(@Param('id', MongoIdPipe) id: string) {
+    return this.clientService.delete(id);
+  }
+}
---
+++ src/modules/cliente/dtos/client.dto.ts
@@ -0,0 +1,49 @@
+import { ApiProperty, PartialType, OmitType } from '@nestjs/swagger';
+import { IsArray, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateClientDto {
+  @ApiProperty({ description: 'Client name.' })
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+  @ApiProperty({ description: 'Client surname.' })
+  @IsString()
+  @IsNotEmpty()
+  readonly surname: string;
+  @ApiProperty({ description: 'Client DNI.' })
+  @IsString()
+  @IsNotEmpty()
+  readonly dni: string;
+  @ApiProperty({ description: 'Client phone number.' })
+  @IsString()
+  @IsNotEmpty()
+  readonly phone: string;
+  @ApiProperty({ description: 'Client email.' })
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+  @ApiProperty({ description: 'Client vehicles list.' })
+  @IsArray()
+  @IsNotEmpty()
+  readonly vehicles: [];
+  @ApiProperty({ description: 'Client details.' })
+  @IsString()
+  readonly detail: string;
+}
+
+export class UpdateClientDTO extends PartialType(
+  OmitType(CreateClientDto, []),
+) {
+  readonly name?: string;
+  readonly surname?: string;
+  readonly dni?: number;
+  readonly phone?: number;
+  readonly email?: string;
+  readonly detail?: string;
+}
+
+export class AddVehiclesToClientDTO {
+  @IsArray()
+  @IsNotEmpty()
+  readonly vehiclesIds: string[];
+}
