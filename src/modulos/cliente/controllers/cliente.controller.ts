25b3d09ac8ff6be69fa63d99314bb49f9d0b72fd
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-10-24 17:34:54 -0300

    ClienteController: Complete responses

--- src/modulos/cliente/controllers/cliente.controller.ts
+++ src/modulos/cliente/controllers/cliente.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Post,
   Put,
@@ -22,20 +24,36 @@ export class ClienteController {
 
   @ApiOperation({ summary: 'Get all clients.' })
   @Get()
-  getClients() {
-    return this.clientService.findAll();
+  @HttpCode(HttpStatus.OK)
+  async getClients() {
+    const clients = await this.clientService.findAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Client listing successful',
+      data: clients,
+    };
   }
 
   @ApiOperation({ summary: 'Get client by ID.' })
   @Get(':id')
-  getClientById(@Param('id') id: string) {
-    return this.clientService.findOneById(id);
+  async getClientById(@Param('id') id: string) {
+    const client = await this.clientService.findOneById(id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Client found successfully',
+      data: client,
+    };
   }
 
   @ApiOperation({ summary: 'Create new client.' })
   @Post()
   createClient(@Body() payload: CreateClientDto) {
-    return this.clientService.create(payload);
+    const newClient = this.clientService.create(payload);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Client created successfully',
+      data: newClient,
+    };
   }
 
   @ApiOperation({ summary: 'Update an existing client by ID.' })
@@ -44,12 +62,34 @@ export class ClienteController {
     @Param('id', MongoIdPipe) id: string,
     @Body() payload: UpdateClientDTO,
   ) {
-    return this.clientService.update(id, payload);
+    const updateClient = this.clientService.update(id, payload);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Client updated successfully',
+      data: updateClient,
+    };
   }
 
   @ApiOperation({ summary: 'Remove a client by ID.' })
   @Delete(':id')
   deleteClient(@Param('id', MongoIdPipe) id: string) {
-    return this.clientService.delete(id);
+    const deletedClient = this.clientService.delete(id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Client deleted successfully',
+      data: deletedClient,
+    };
   }
+
+  /* @ApiOperation({ summary: 'Get clients ordered by A-Z.' })
+  @Get('orderAZ')
+  async getClientsOrdered() {
+    const clients = await this.clientService.findAll();
+    clients.sort((a, b) => a.surname.localeCompare(b.surname));
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Client sorted A-Z successful',
+      data: clients,
+    };
+  } */
 }
